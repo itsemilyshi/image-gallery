677fcfdad08fdb2aa45af0e1e767469eba2f20d5
Emily Shi <emilyshi@Emilys-MBP.hitronhub.home>
2020-09-03 21:26:33 -0400

    cart stuff

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,21 @@
 class CartController < ApplicationController
+
+    def add_to_cart
+        if current_user.cart == nil
+            cart = Cart.create(user_id: current_user.id)
+        else
+            cart = Cart.find(user_id: current_user.id)
+        end
+
+        cart.photos << photo
+        cart.save #not needed?? 
+    end
+
+    def remove_from_cart
+        cart.photos.delete(photo)
+    end
+
+    def checkout 
+        current_user.money = current_user.money - cart.price
+    end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,12 @@
 class Cart < ApplicationRecord
     belongs_to :user
     has_many :photos
+
+    def cost
+        price = 0
+        cart.photos.each do |photo|
+            price = price + (photo.price * (photo.discount / 100))
+        end
+        return cost
+    end
 end
