e2d3a5d500e7de05d0b62e74e77b1b6650ff8f10
Emily Shi <emilyshi@Emilys-MBP.hitronhub.home>
2020-09-04 14:23:41 -0400

    change cart to have before action

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,21 +1,26 @@
 class CartController < ApplicationController
+    before_action :set_cart
 
     def add_to_cart
-        byebug
-        if current_user.cart == nil
-            cart = Cart.create(user_id: current_user.id)
-        else
-            cart = Cart.find_by_user_id(current_user.id)
-        end
         photo = Photo.find(params[:photo_id])
-        cart.photos << photo
+        @cart.photos << photo
     end
 
     def remove_from_cart
-        cart.photos.delete(@photo)
+        @cart.photos.delete(photo)
     end
 
     def checkout 
-        current_user.money = current_user.money - cart.price
+        current_user.money = current_user.money - @cart.price
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cart
+        if current_user.cart == nil
+            @cart = Cart.create(user_id: current_user.id)
+        else
+            @cart = current_user.cart
+        end    
     end
 end
