98527e4684e8b8acff1e0cd7331ae476471b8b3f
Emily Shi <emilyshi@Emilys-MBP.hitronhub.home>
2020-09-05 17:03:07 -0400

    remove from cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,17 +1,37 @@
 class CartController < ApplicationController
     before_action :set_cart
 
+    def index
+        @current_user = current_user
+    end
+
+    def show
+        respond_to do |format|
+            format.html {render :index}
+        end
+    end
+
     def add_to_cart
         photo = Photo.find(params[:photo_id])
         @cart.photos << photo
+        respond_to do |format|
+            format.html {redirect_to @cart}
+        end
     end
 
     def remove_from_cart
+        photo = Photo.find(params[:photo_id])
         @cart.photos.delete(photo)
+        respond_to do |format|
+            format.html {redirect_to @cart}
+        end
     end
 
     def checkout 
         current_user.money = current_user.money - @cart.price
+        respond_to do |format|
+            format.html {redirect_to @cart}
+        end
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'add_to_cart/:photo_id', to: "cart#add_to_cart", as: :add_to_cart
+  get 'remove_from_cart/:photo_id', to: "cart#remove_from_cart", as: :remove_from_cart
+  get 'checkout', to: "cart#checkout"
 
   root to: 'photos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
